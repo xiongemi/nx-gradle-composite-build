8e678a00d983764366bd17289cfe97c614a6edfd
Emily Xiong <xiongemi@gmail.com>
2024-11-22 14:39:27 -0500

    nx init

--- my-app/build.gradle.kts
+++ my-app/build.gradle.kts
@@ -1,14 +1,3 @@
-defaultTasks("run")
-
-tasks.register("run") {
-    dependsOn(gradle.includedBuild("my-app").task(":app:run"))
-}
-
-tasks.register("checkAll") {
-    dependsOn(gradle.includedBuild("my-app").task(":app:check"))
-    dependsOn(gradle.includedBuild("my-utils").task(":number-utils:check"))
-    dependsOn(gradle.includedBuild("my-utils").task(":string-utils:check"))
-}
 
 allprojects {
   apply {
